a829883492294432601dfead5c72645a2f3cec14
chueunjeong <miho7724@gmail.com>
2020-09-08 15:01:06 +0900

    [chu]add uploadRouter.js

--- index.js
+++ index.js
@@ -11,24 +11,32 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // 몽고 디비 연결
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false})
-    .then(() => console.log('connected to mongodb'))
-    .catch(e => console.error(e));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("connected to mongodb"))
+  .catch((e) => console.error(e));
 
 // 라우터 설정
-const testRouter = require('./routes/testRouter');
-const menuRouter = require('./routes/menuRouter');
-const storeRouter = require('./routes/storeRouter');
+const testRouter = require("./routes/testRouter");
+const menuRouter = require("./routes/menuRouter");
+const storeRouter = require("./routes/storeRouter");
 const kakaoRouter = require("./routes/kakaoRouter");
-const orderRouter = require('./routes/orderRouter');
-const pointRouter = require('./routes/pointRouter');
+const orderRouter = require("./routes/orderRouter");
+const pointRouter = require("./routes/pointRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
-app.use('/api', testRouter);
-app.use('/api/menu', menuRouter);
-app.use('/api/store', storeRouter);
+app.use("/api", testRouter);
+app.use("/api/menu", menuRouter);
+app.use("/api/store", storeRouter);
 app.use("/api/kakao", kakaoRouter);
-app.use('/api/order', orderRouter);
-app.use('/api/point', pointRouter);
+app.use("/api/order", orderRouter);
+app.use("/api/point", pointRouter);
+app.use("/api/upload", uploadRouter);
 
 // 서버 시작
 app.listen(port, () => {
